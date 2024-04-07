335f8ec74bf828a4df179bb3892795c1798f0d0b
armawwnn <sadeghi.ali.1999@gmail.com>
2024-04-07 19:57:22 +0330

    climbing

--- hill_climbing.py
+++ hill_climbing.py
@@ -2,9 +2,25 @@ from functions import successor,count_threats
 
 
 
-start_state = [3,3,0,1]
+start_state = [1,3,1,1]
 neigohbor = successor(start_state)
 
-for j in range(0,2):
-    print(neigohbor[j])
-    
+def climbing(start):
+    start_node = start
+    while True:
+        nei = successor(start_node)
+        if count_threats(nei[0]) <= count_threats(nei[1]) and count_threats(nei[0]) < count_threats(start_node):
+            start_node = nei[0]
+            print(start_node)
+         
+        if count_threats(nei[1]) <= count_threats(nei[0]) and count_threats(nei[1]) < count_threats(start_node):
+            start_node =  nei[1]
+            print(start_node)
+
+        else:
+            return start_node
+
+resault = climbing(start_state)
+num = count_threats(resault)
+print("resault : ")
+print(resault,num)
